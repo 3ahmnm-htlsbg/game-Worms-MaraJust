b451a49cf9bfc758c0bf9f2994303c6c4d2fa784
Mara Just <marajust@mjust03-MacBook-Pro.local>
2020-02-18 14:33:10 +0100

    config addForce and rigidbody

--- Assets/MyGame/Scripts/WormControler.cs
+++ Assets/MyGame/Scripts/WormControler.cs
@@ -6,33 +6,58 @@ public class WormControler : MonoBehaviour
 {
     // D (Declare Variable) A (Assign Value) R (Refer to other Code) E (Evaluate Condition)
 
-    public int x;
-    public string text;
+    //Schritt für Schritt
+    //access modifier public oder private
+    //deklarieren der Klasse
+    //MonoBehavior ist Überklasse; wir machen Unterklasse davon und können FUnktionen aus dieser Bibliothek hinzufügen.
+    //
+
+    //public object Worm;
+    public int x = 1;
+    public float speed = 10f;
+    public Rigidbody Rigidbody_Worm;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (x > 0)
-        {
-            Debug.Log("x ist größer als 0");
-        }
-        else
-        {
-            Debug.Log("x ist 0 oder kleiner als 0");
-        }
-
-        if (text == "MaraJust")
-        {
-            Debug.Log("In der text variable steht MaraJust");
-        }
-        else
-        {
-            Debug.Log("In der text variable steht etwas anderes");
-        }
+        Rigidbody_Worm = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+
+            Debug.Log("E ist gedrückt!");
+            Rigidbody_Worm.AddForce(transform.right * speed);
+
+
+        }
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.Log("Q ist gedrückt!");
+            Rigidbody_Worm.AddForce(-transform.right * speed);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W ist gedrückt!");
+            Rigidbody_Worm.AddForce(transform.up * speed);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space ist gedrückt!");
+        }
+
 
     }
 }
